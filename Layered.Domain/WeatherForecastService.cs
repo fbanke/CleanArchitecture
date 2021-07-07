29c966c7c58322c77e88f6ffbf6c8cf202f8d9b6
Frederik Banke <frederik@patch.dk>
2021-07-07 15:29:29 +0200

    feat: added layered architecture example

---
+++ Layered.Domain/WeatherForecastService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Layered.Infrastructure;
+
+namespace Layered.Domain
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly WeatherForecastApi _weatherForecastApi;
+
+        public WeatherForecastService(WeatherForecastApi weatherForecastApi)
+        {
+            _weatherForecastApi = weatherForecastApi;
+        }
+
+        public IEnumerable<WeatherForecast> Get(int daysToForecast)
+        {
+            for (var i = 0; i < daysToForecast; i++)
+            {
+                var weatherForecast = _weatherForecastApi.Get(i);
+                
+                yield return new WeatherForecast
+                {
+                    Date = weatherForecast.Date,
+                    TemperatureC = weatherForecast.TemperatureC,
+                    Summary = Summaries[weatherForecast.Summary]
+                };
+            }
+        }
+    }
+}
---
+++ Layered.Infrastructure/WeatherForecastApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Layered.Infrastructure
+{
+    public class WeatherForecastApi
+    {
+        public WeatherForecastDao Get(int daysAhead)
+        {
+            var rng = new Random();
+            var day = DateTime.Now;
+            day = day.AddDays(daysAhead);
+
+            return new WeatherForecastDao
+            {
+                Date = day,
+                TemperatureC = rng.Next(-20, 55),
+                Summary = rng.Next(10)
+            };
+        }
+    }
+}
